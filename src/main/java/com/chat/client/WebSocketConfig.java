bd0136e87b134b2b06707fb1913cbbc813ea4692
Agaev Artem <Agaev.A.R@sberbank.ru>
2020-05-19 22:20:14 +0300

    + auth part

--- src/main/java/com/chat/client/WebSocketConfig.java
+++ src/main/java/com/chat/client/WebSocketConfig.java
@@ -3,8 +3,6 @@ package com.chat.client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
@@ -18,9 +16,5 @@ public class WebSocketConfig {
         stompClient.setMessageConverter(new StringMessageConverter());
         return stompClient;
     }
-
-//    public SimpMessagingTemplate sendingOperations() {
-//        return new SimpMessagingTemplate();
-//    }
 
 }
--- src/main/java/com/chat/client/web/StreamController.java
+++ src/main/java/com/chat/client/web/StreamController.java
@@ -1,22 +1,16 @@
 package com.chat.client.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import java.io.Reader;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -30,7 +24,15 @@ public class StreamController {
 
     @GetMapping("connect")
     public void connect(@RequestParam("message") String message) throws ExecutionException, InterruptedException {
-        ListenableFuture<StompSession> connect = client.connect("ws://localhost:8080/connect", new SessionHandler());
+        StompHeaders headers = new StompHeaders();
+        headers.add("login", "client-1");
+        headers.add("passcode", "super-secret");
+        ListenableFuture<StompSession> connect = client.connect(
+                "ws://localhost:8080/connect",
+                new WebSocketHttpHeaders(),
+                headers,
+                new SessionHandler()
+        );
         this.session = connect.get();
     }
 
